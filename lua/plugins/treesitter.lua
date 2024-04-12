cd6051052ebff7315e27be529875c6213e7473ee
Timothy Dowling <timothy_dowling@me.com>
2024-04-12 16:11:22 -0400

    Added completion

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "javascript" },
+      ensure_installed = { "lua", "javascript" },
       highlight = { enable = true },
       indent = { enable = true },  
     })
