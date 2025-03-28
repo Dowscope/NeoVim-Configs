cf6191341927d4606dcb55069a70a0268580263f
Timothy Dowling <timothy.dowling@me.com>
2025-03-27 21:03:21 -0400

    Updated NeoTree

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -20,7 +20,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.lua_ls.setup({
@@ -34,7 +34,7 @@ return {
       })
       lspconfig.csharp_ls.setup({
         capabilities = capabilities
-      }) 
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
